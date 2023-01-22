58ef0920ce7642701a813ddf58eda934e85f8c28
KujouRinka <kujourinka@gmail.com>
2023-01-22 19:21:15 +0800

    lab thread: uthread

--- user/uthread.c
+++ user/uthread.c
@@ -1,5 +1,6 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/riscv.h"
 #include "user/user.h"
 
 /* Possible states of a thread: */
@@ -10,15 +11,34 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context ctx;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
+// thread_switch(saver, reader)d
               
 void 
 thread_init(void)
@@ -63,6 +83,9 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    // TODO: call thread_switch
+    t->ctx.ra = r_ra();
+    thread_switch((uint64)&t->ctx, (uint64)&current_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -77,6 +100,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->ctx.ra = (uint64)func; // TODO: ??
+  t->ctx.sp = (uint64)t->stack + STACK_SIZE;
 }
 
 void 
