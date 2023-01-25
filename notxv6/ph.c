00f44436cb61cef90970f58621d182566e6735fd
KujouRinka <kujourinka@gmail.com>
2023-01-25 11:03:44 +0800

    lab thread: use thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t bucket_lock[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -42,6 +43,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(&bucket_lock[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -53,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&bucket_lock[i]);
 }
 
 static struct entry*
@@ -62,9 +65,11 @@ get(int key)
 
 
   struct entry *e = 0;
+  pthread_mutex_lock(&bucket_lock[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  pthread_mutex_unlock(&bucket_lock[i]);
 
   return e;
 }
@@ -113,6 +118,11 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  // init mutex
+  for (int i = 0; i < NBUCKET; ++i) {
+    pthread_mutex_init(&bucket_lock[i], NULL);
   }
 
   //
